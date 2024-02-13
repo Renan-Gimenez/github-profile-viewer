3c8cbb082b9cec0b947ec9d08f87f8ae62386562
Renan Gimenez <renangiimenez@gmail.com>
2024-02-12 23:52:08 -0300

    feat: implement main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,54 @@
+"use client";
+
+import { useState } from "react";
+
+import { SearchBar } from "@/components/SearchBar";
+import { UserProfile } from "@/components/UserProfile";
+import { RepoList } from "@/components/RepoList";
+
+import { FirstPage } from "@/components/FirstPage";
+import { RepoNotFound } from "@/components/RepoNotFound";
+import { UserNotFound } from "@/components/UserNotFound";
+
+import { Loader } from "lucide-react";
+import { useFetchProfile, useFetchRepos } from "@/services/githubService";
+
 export default function Home() {
+  const [username, setUsername] = useState("");
+
+  const { data: profileData, isLoading: isloadingProfile } =
+    useFetchProfile(username);
+  const { data: reposData, isLoading: isLoadingRepos } =
+    useFetchRepos(username);
+
+  if (isloadingProfile || isLoadingRepos) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center ring ring-red-500 bg-[#121214]">
+        <Loader className="h-10 w-auto text-white animate-spin" />
+      </div>
+    );
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center bg-[#121214] p-36">
+      <SearchBar setUsername={setUsername} />
+
+      {profileData ? (
+        <>
+          <UserProfile user={profileData} />
+          {reposData && reposData.length > 0 ? (
+            <div className="w-[448px] rounded-2xl bg-[#202024]">
+              <RepoList repos={reposData} />
+            </div>
+          ) : (
+            <RepoNotFound />
+          )}
+        </>
+      ) : username ? (
+        <UserNotFound username={username} />
+      ) : (
+        <FirstPage />
+      )}
     </main>
   );
 }
