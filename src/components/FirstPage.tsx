1116bf66bfd0e883d04360cf4dd56b92daff4d07
Renan Gimenez <renangiimenez@gmail.com>
2024-02-12 23:47:32 -0300

    chore: create first page screen

---
+++ src/components/FirstPage.tsx
@@ -0,0 +1,10 @@
+import { Search } from "lucide-react";
+
+export function FirstPage() {
+  return (
+    <div className="flex flex-col items-center gap-4 p-12">
+      <Search className="h-16 w-16" />
+      <span>Search for some GitHub account</span>
+    </div>
+  );
+}
